f10ca54293a5a983dacfc2e5fbcf910c69b78422
Cappillen Lee <cappillen@ucsb.edu>
2024-01-18 17:51:50 -0800

    pass tests

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from calee14!";
+        return "Greetings from Spring Boot!";
     }
     
 }
